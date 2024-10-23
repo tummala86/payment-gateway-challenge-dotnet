f1b6bf196d064863474cf74b771c5f49d009f569
tummala86@gmail.com <tummala86@gmail.com>
2024-10-23 15:58:49 +0100

    Create payment failure integration test issue fix

--- test/PaymentGateway.Test.Integration/CreatePayment/FailureTests.cs
+++ test/PaymentGateway.Test.Integration/CreatePayment/FailureTests.cs
@@ -12,31 +12,25 @@ namespace PaymentGateway.Test.Integration.CreatePayment
         public async Task CreatePayment_ShouldReturnInvalidParameters_WhenRequestIsInvalid()
         {
             // Arrange
-            var idempotencyKey = Guid.NewGuid().ToString();
             var client = Server.CreateClient();
 
-            var paymentRequest = PaymentRequest(0);
+            var postPaymentRequest = new
+            {
+                cardNumber = "2222405343248877",
+                expiryMonth = 4,
+                expiryYear = 2025,
+                currency = "GBP",
+                amount = 0,
+                cvv = "123"
+            };
 
             // Act
-            var response = await client.PostAsJsonAsync("/payments", paymentRequest);
+            var response = await client.PostAsJsonAsync("/payments", postPaymentRequest);
 
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
             var body = await response.Content.ReadAsStringAsync();
             body.Should().NotBeNull();
-        }
-
-        private object PaymentRequest(int amount)
-        {
-            return new
-            {
-                card_number = "2222405343248877",
-                expiry_month = 4,
-                expiry_year = 2025,
-                currency = "gbp",
-                amount = amount,
-                cvv = "123"
-            };
         }
     }
 }
